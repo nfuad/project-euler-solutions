903302ab1f877f38bf021823915b75f7162283d7
Nafis Fuad <fuadbd14@gmail.com>
2019-11-22 06:22:41 +0600

    prob-17 solution

--- prob-17.js
+++ prob-17.js
@@ -2,18 +2,18 @@
 // If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
 // NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
 
-const lenHundred = 7;
-const lenThousand = 8;
-const lenPlaceOnes = [0, 3, 3, 5, 4, 4, 3, 5, 5, 4];
-const lenPlaceTens = [0, 3, 6, 6, 5, 5, 5, 7, 6, 6];
+const lenHundred = 7; // length of the string "hundred"
+const lenThousand = 8; // length of the string "thousand"
+const lenPlaceOnes = [0, 3, 3, 5, 4, 4, 3, 5, 5, 4]; // length of the strings "one", "two", "three", ... "nine", respectively
+const lenPlaceTens = [0, 3, 6, 6, 5, 5, 5, 7, 6, 6]; // length of the strings "ten", "twenty", "thirty" ... "ninety" respectively
 const num = 1000;
 let sum = 0;
 
 for (let i = 1; i <= num; i++) {
-    let placeOnes = i % 10;
-    let placeTens = Math.floor(i / 10) % 10;
-    let placeHundreds = Math.floor(i / 100) % 10;
-    let placeThousands = Math.floor(i / 1000) % 10;
+    let placeOnes = i % 10; // get the ones place of the give number
+    let placeTens = Math.floor(i / 10) % 10; // get the tens place of the give number
+    let placeHundreds = Math.floor(i / 100) % 10; // get the hundreds place of the give number
+    let placeThousands = Math.floor(i / 1000) % 10; // get the thousands place of the give number
     console.log(placeThousands, placeHundreds, placeTens, placeOnes)
 
     // Add the ones place
