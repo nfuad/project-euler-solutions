1cebbf8099f9724c61ba2f72b690386eeb82a75d
Nafis Fuad <fuadbd14@gmail.com>
2019-12-02 18:30:04 +0600

    prob-21 solution

--- prob-21.js
+++ prob-21.js
@@ -1,44 +1,36 @@
-// Let d(n) be defined as the sum of proper divisors of n (numbers less than n which 
-// divide evenly into n). If d(a) = b and d(b) = a, where a ≠ b, then a and b are an 
-// amicable pair and each of a and b are called amicable numbers. For example, the 
-// proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore 
+// Let d(n) be defined as the sum of proper divisors of n (numbers less than n which
+// divide evenly into n). If d(a) = b and d(b) = a, where a ≠ b, then a and b are an
+// amicable pair and each of a and b are called amicable numbers. For example, the
+// proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore
 // d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 // Evaluate the sum of all the amicable numbers under 10000.
 
-let factorSum1 = 0
-let factorSum2 = 0
-let output = 0
-
-for (let i = 1; i <= 10000; i++) {
-    for (let j = 1; j < i; j++) {
-        // add to the sum of factors if proper divisor
-        if (i % j === 0) factorSum1 += j
+// get an array of all the proper divisors
+const getProperFactors = n => {
+  const divisors = [];
+  const root = Math.floor(Math.sqrt(n));
+  for (let i = 1; i <= root; i++) {
+    if (n % i === 0) {
+      divisors.push(i);
+      if (Math.pow(i, 2) !== n) divisors.push(n / i);
     }
+  }
+  return divisors.sort((a, b) => a - b).filter(x => x !== n);
+};
 
-    for (let k = 1; k < factorSum1; k++) {
-        // add to the sum of proper divisors
-        if (factorSum1 % k === 0) factorSum2 += k
-    }
+// let d(n) be defined as the sum of proper divisors of
+// n (numbers less than n which divide evenly into n).
+const d = n => getProperFactors(n).reduce((a, b) => a + b, 0);
 
-    // make sure numbers are unique
-    (factorSum2 === i && factorSum2 != factorSum1) ? output += (factorSum2 + i) : null
+// iterate over all numbers from 0 to 9999.
+const result = Array.from({ length: 9999 })
+  .map((_, n) => {
+    const a = d(n);
+    const b = d(a);
+    // If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable
+    // pair and each of a and b are called amicable numbers.
+    return n !== a && n === b ? a : 0;
+  })
+  .reduce((a, b) => a + b);
 
-    // reset values to 0
-    factorSum1 = 0;
-    factorSum2 = 0;
-}
-
-// divide by 2 so that each pair's counted once.
-output = output / 2;
-
-console.log(output)
-
-
-
-// ALTERNATIVE METHOD THAT MIGHT WORK
-// https://en.wikipedia.org/wiki/Amicable_numbers
-// Euler's rule is a generalization of the Thâbit ibn Qurra theorem. It states that if
-// p = (2^(n − m) + 1)×2^m − 1,
-// q = (2^(n − m) + 1)×2^n − 1,
-// r = (2^(n − m) + 1)2×2^(m + n) − 1,
-// where n > m > 0 are integers and p, q, and r are prime numbers, then 2^n×p×q and 2^n×r are a pair of amicable numbers.
+console.log(result);
